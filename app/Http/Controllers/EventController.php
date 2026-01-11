72174bde0e0f7101078ef6b7d181575e638d6f50
danishdanial0303 <danish03@graduate.utm.my>
2026-01-12 02:18:51 +0800

    Update EventController.php

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -198,7 +198,7 @@ class EventController extends Controller
     {
         $image =  $request->file('event_image');
         if ($image) {
-            $imagePath =  Storage::disk('s3')->putFile('event_images', $image, 'public');
+            $imagePath =  Storage::disk('s3')->putFile('event_images', $image);
             $request->merge(['event_image_url' => Storage::disk('s3')->url($imagePath)]);
         }
         
