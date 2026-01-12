b05ad26f690a79cfb6cb29639562f5d48e396d10
danishdanial0303 <danish03@graduate.utm.my>
2026-01-13 02:20:02 +0800

    Update AuthController.php

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -95,7 +95,7 @@ class AuthController extends Controller
             ['id' => $user->id, 'hash' => sha1($user->email)]
         );
 
-        Mail::to($user->email)->send(new VerifyEmailMail($url));
+        Mail::to($user->email)->queue(new VerifyEmailMail($url));
 
         return response()->json(['message' => 'Verification email sent']);
     }
